c1891eac7b114d6c1541432aaea93521d1c6c261
Josh Steele <josh@steelethis.com>
2016-01-06 00:22:55 -0500

    Algorithm written and working.

---
+++ CollatzConjecture/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzConjecture
+{
+    class Program
+    {
+        private static void Main()
+        {
+            int n = 0;
+            int steps = 0;
+
+            Console.WriteLine("******** COLLATZ CONJECTURE ********\n\n\n");
+            Console.Write("Enter a number greater than 1>>> ");
+            string number = Console.ReadLine();
+
+            try
+            {
+                n = Int32.Parse(number);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input, restarting...");
+                Main();
+            }
+
+            while (n != 1)
+            {
+                if (n%2 == 0)
+                {
+                    n = n/2;
+                }
+                else
+                {
+                    n = n*3 + 1;
+                }
+                steps++;
+            }
+
+            Console.WriteLine($"It took {steps} steps to reach {n} from {number}");
+            Console.ReadKey();
+        }
+    }
+}
